47c139512e72130fdb9708b5891abb24aee31cdf
sjetti5555 <183280305+sjetti5555@users.noreply.github.com>
2025-02-21 17:22:08 -0500

    Update app6.py

--- youtubeapptesting/1youtubeapp/app6.py
+++ youtubeapptesting/1youtubeapp/app6.py
@@ -12,7 +12,7 @@ nltk.download('punkt')
 nltk.download('stopwords')
 
 # Initialize YouTube API
-api_key = 'AIzaSyCuM07jYUqnGTjncnNop5TLz8vgZ0vSKWQ'  # Replace with your YouTube Data API key
+api_key = 'Youtube_Api_key'  # Replace with your YouTube Data API key
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 # Initialize sentiment analysis model
